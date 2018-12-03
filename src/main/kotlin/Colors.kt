bdb9fba693ae16273c6781f5cfec03f004348ba7
Thomas Prebeck <thomas.prebeck@oth-regensburg.de>
2018-12-03 19:54:47 +0100

    work

--- src/main/kotlin/Colors.kt
+++ src/main/kotlin/Colors.kt
@@ -6,6 +6,7 @@ import kotlinx.cinterop.CPointer
 
 class Color(val fmt: CPointer<SDL_PixelFormat>) {
   val BLUE = SDL_MapRGB(fmt, 0, 0, 255)
-  val RED = SDL_MapRGB(fmt, 255, 0, 0)
   val GREEN = SDL_MapRGB(fmt, 0, 255, 0)
+  val RED = SDL_MapRGB(fmt, 255, 0, 0)
+  val WHITE = SDL_MapRGB(fmt, 255, 255, 255)
 }
--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,13 +1,17 @@
 package main
 
 import kotlinx.cinterop.CPointer
+import kotlinx.cinterop.pointed
 
+import sdl.SDL_FillRect
 import sdl.SDL_Surface
 
 class Game(surface: CPointer<SDL_Surface>) {
   val bricks = mutableListOf<Brick>()
   val paddle = Paddle(surface, 350, 500)
   val ball = Ball(surface, 355, 495)
+  val surface = surface
+  val backgroundColor = Color(surface.pointed.format!!).WHITE
   init {
     (0..5).forEach { y ->
       (0..15).forEach { x ->
@@ -16,8 +20,16 @@ class Game(surface: CPointer<SDL_Surface>) {
     }
   }
   fun draw() {
+    SDL_FillRect(surface, null, backgroundColor)
     bricks.forEach { it.draw() }
     paddle.draw()
     ball.draw()
+  }
+  fun update(mode: Mode) {
+    when (mode) {
+      Mode.Left -> paddle.moveX(-1)
+      Mode.Right -> paddle.moveX(1)
+      else -> null
+    }
   }
 }
