cd1ff735ca592d29446001fd3f184e4d704bf75c
Thomas Prebeck <thomas.prebeck@oth-regensburg.de>
2018-12-03 21:36:25 +0100

    work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package main
 
+import sdl.SDL_AddTimer
 import sdl.SDL_CreateWindow
 import sdl.SDL_DestroyWindow
 import sdl.SDL_Event
@@ -8,14 +9,21 @@ import sdl.SDL_Init
 import sdl.SDL_INIT_VIDEO
 import sdl.SDL_KEYUP
 import sdl.SDL_PollEvent
+import sdl.SDL_PushEvent
+import sdl.SDL_RemoveTimer
 import sdl.SDL_UpdateWindowSurface
 import sdl.SDL_WINDOWPOS_CENTERED
 import sdl.SDL_Quit
 
 import kotlinx.cinterop.Arena
 import kotlinx.cinterop.alloc
+import kotlinx.cinterop.COpaquePointer
 import kotlinx.cinterop.ptr
+import kotlinx.cinterop.staticCFunction
 
+fun cb(interval: UInt, param: COpaquePointer?): UInt {
+  return interval
+}
 fun main(args: Array<String>) {
   val arena = Arena()
   SDL_Init(SDL_INIT_VIDEO)
@@ -31,6 +39,8 @@ fun main(args: Array<String>) {
   arena.defer { SDL_DestroyWindow(win) }
   var run = true
   val event = arena.alloc<SDL_Event>()
+  val timer = SDL_AddTimer(100 / 6, staticCFunction(::cb), null)
+  arena.defer { SDL_RemoveTimer(timer) }
   while (run) {
     SDL_PollEvent(event.ptr)
     when (event.type) {
